d20711c46f7db284b39c68bd38d48cddb0454317
Alex Calia <alex.m.calia@outlook.com>
2020-12-08 12:41:03 -0500

    working with useState

--- src/index.js
+++ src/index.js
@@ -1,53 +1,29 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = ({name, age}) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear();
-    return yearNow - age
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
 
   return (
     <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probbaly born in {bornYear()}</p>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <>
-      <p>Greeting app created by <a href="https://alexcalia.com">Alex Calia</a></p>
-    </>
-  )
-}
-
-const Display = ({counter}) => <div>{counter}</div>;
-
-const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>;
-
-const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  const increaseByOne = () => setCounter(counter + 1);
-  const setToZero = () => setCounter(0);
-  const decreaseByOne = () => setCounter(counter - 1);
-
-  const name = "Peter";
-  const age = 10;
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10} />
-      <Hello name={name} age={age} />
-      <Footer />
-      <Button handleClick={increaseByOne} text="Plus" />
-      <Button handleClick={decreaseByOne} text="Minus" />
-      <Button handleClick={setToZero} text="Reset" />
-      <Display counter={counter} />
-    </>
   )
 }
 
