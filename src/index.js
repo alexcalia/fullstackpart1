0e427638c09637944c996efee8b430995f15b5d8
Alex Calia <alex.m.calia@outlook.com>
2020-12-16 11:36:29 -0500

    created helper functions to render forms conditionally

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ const App = () => {
       })
   }
 
-  const handleChange = (event) => {
+  const handleNoteChange = (event) => {
     setNewNote(event.target.value);
   }
 
@@ -98,32 +98,49 @@ const App = () => {
     }
   }
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>  
+  )
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
 
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      {user === null ?
+      loginForm() :
+      noteForm()
+      }
 
       <div>
         <button onClick={() => setShowAll(!showAll)}>
